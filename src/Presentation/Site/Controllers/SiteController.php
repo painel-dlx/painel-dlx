fc50256da2f7a6904777e8598085088407948373
dlepera <dlepera88@gmail.com>
2018-12-20 22:00:25 -0200

    Utilizando o plugin jquery-form-ajax

--- src/Presentation/Site/Controllers/SiteController.php
+++ src/Presentation/Site/Controllers/SiteController.php
@@ -46,7 +46,8 @@ abstract class SiteController
         $this->commandBus = $commandBus;
 
         // TODO: retirar a inclusão do tema do controller. Está aqui apenas para agilizar o dev
-        $this->view->addArquivoCss('/src/Presentation/Site/Temas/Painel-DLX/css/paineldlx.tema.css');
-        $this->view->addArquivoJs('/src/Presentation/Site/Temas/Painel-DLX/js/paineldlx.tema.js');
+        $this->view->addArquivoCss('/src/Presentation/Site/public/temas/painel-dlx/css/paineldlx.tema.css');
+        $this->view->addArquivoJs('/src/Presentation/Site/public/temas/painel-dlx/js/paineldlx.tema.js');
+        $this->view->addArquivoJs('/src/Presentation/Site/public/js/painel-dlx.js');
     }
 }
---
+++ src/Presentation/Site/public/js/painel-dlx.js
@@ -0,0 +1,20 @@
+$('.menu-lateral-item').on('click', function () {
+    var $this = $(this);
+    var $menu_lateral = $this.parents('.menu-lateral');
+    var ocultar = [];
+    var mostrar = $this.data('mostrar-html');
+
+    if (!$this.hasClass('ativo')) {
+        $menu_lateral.find('.menu-lateral-item[data-mostrar-html]').each(function () {
+            ocultar.push($(this).data('mostrar-html'));
+        });
+
+        ocultar = ocultar.join(', ');
+
+        $(ocultar).hide();
+        $(mostrar).show();
+
+        $menu_lateral.find('.menu-lateral-item.ativo').removeClass('ativo');
+        $this.addClass('ativo');
+    }
+});
